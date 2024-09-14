0424c8d44ce88f7285f96cfec661fd2fcb8a2f63
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-09-14 13:46:03 -0500

    update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,3 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("active");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
